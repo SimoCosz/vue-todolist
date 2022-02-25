4ede920c9f33739547f69124e8fda0b1329caa4a
coszach@hotmail.it <coszach@hotmail.it>
2022-02-25 14:08:02 +0100

    creata repositori+stile+listItem

---
+++ app.js
@@ -0,0 +1,19 @@
+const app = new Vue({
+  el: '#app',
+
+  data:{
+    listItem: 
+    {
+      text: 'Fare la spesa',
+      done: 'false'
+    },
+    {
+      text: 'Fare L\'esercizio di boolean',
+      done: 'false'
+    },
+    {
+      text: 'Ricordarsi di pushare spesso',
+      done: 'false'
+    },
+  }
+})
