8c8303644f1f2eeb179c94067a167d3c9d1adebd
coszach@hotmail.it <coszach@hotmail.it>
2022-02-25 15:13:42 +0100

    milstone 3 e bonus 1 completati

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const app = new Vue({
   el: '#app',
 
   data:{
+    newItem: '',
     listItem: [
       {
         text: 'Fare la spesa',
@@ -20,12 +21,22 @@ const app = new Vue({
 
   methods: {
     changeDone: function(item){
-      item.done = true
-      console.log('click')
+      item.done = true;
+      console.log('click');
     },
     
     deleteItem: function(i){
-      this.listItem.splice(i, 1)
-    }
+      this.listItem.splice(i, 1);
+    },
+    addItem: function(newItem){
+      if(this.newItem !== ''){
+        const item = {
+          text: this.newItem,
+          done: false,
+        };
+        this.listItem.push(item);
+        this.newItem = '';
+      }
+    },
   }
 })
